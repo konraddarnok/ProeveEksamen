5036d2d399f42378df1f97e98a777bd89d226df2
konraddarnok <35291936+konraddarnok@users.noreply.github.com>
2020-06-02 12:39:35 +0200

    OPG 1 Static Metoder

--- Program.cs
+++ Program.cs
@@ -1,35 +1,12 @@
 ﻿using System;
-using System.IO;
-using System.Net.Sockets;
 
-namespace OPG3TCPCLIENT
+namespace opg1
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello World it is client!");
-            Console.ReadLine();
-
-
-            TcpClient clientSocket = new TcpClient("localhost", 7000);
-
-
-            Stream ns = clientSocket.GetStream();  //provides a Stream
-            StreamReader sr = new StreamReader(ns);
-            StreamWriter sw = new StreamWriter(ns);
-            sw.AutoFlush = true; // enable automatic flushing
-
-            string message = Console.ReadLine();
-            sw.WriteLine(message);
-            string serverAnswer = sr.ReadLine();
-            Console.WriteLine("Server: " + serverAnswer);
-
-            ns.Close();
-
-            clientSocket.Close();
+            Console.WriteLine("Hello World!");
         }
     }
-
 }
